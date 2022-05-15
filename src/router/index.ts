358a0d02954753374d052e8590001241199a8299
Cao Zhijie <1051029537@qq.com>
2022-05-15 15:41:59 +0800

    feat  404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,24 +3,51 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
-    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue"),
-  },
-  {
-    path: "/pathfinding",
-    name: "pathfinding",
+    name: "root",
     component: () =>
-      import(/* webpackChunkName: "pathfinding" */ "../views/Pathfinding.vue"),
-  },
-  {
-    path: "/sort",
-    name: "sort",
-    component: () => import(/* webpackChunkName: "sort" */ "../views/Sort.vue"),
-  },
-  {
-    path: "/test",
-    name: "test",
-    component: () => import(/* webpackChunkName: "Test" */ "../views/Test.vue"),
+      import(/* webpackChunkName: "Layout" */ "../components/Layout.vue"),
+    redirect: "home",
+    children: [
+      {
+        path: "home",
+        name: "home",
+        component: () =>
+          import(/* webpackChunkName: "home" */ "../views/Home.vue"),
+      },
+      {
+        path: "pathfinding",
+        name: "pathfinding",
+        component: () =>
+          import(
+            /* webpackChunkName: "pathfinding" */ "../views/pathfinding/Pathfinding.vue"
+          ),
+      },
+      {
+        path: "sort",
+        name: "sort",
+        component: () =>
+          import(/* webpackChunkName: "sort" */ "../views/Sort.vue"),
+      },
+      {
+        path: "test",
+        name: "test",
+        component: () =>
+          import(/* webpackChunkName: "Test" */ "../views/Test.vue"),
+      },
+      {
+        path: "canvas",
+        name: "canvas",
+        component: () =>
+          import(
+            /* webpackChunkName: "Test" */ "../views/pathfinding/CanvasMap.vue"
+          ),
+      },
+      {
+        path: "/:pathMatch(.*)",
+        name: "404",
+        component: () => import("@/views/404.vue"),
+      },
+    ],
   },
 ];
 
---
+++ src/views/404.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex flex-col justify-center page-container">
+    <div class="text-center">
+      <img src="@/assets/images/404.svg" />
+    </div>
+    <div class="text-center">
+      <p class="text-base">抱歉，你访问的页面不存在</p>
+      <n-button type="info" @click="goHome">回到首页</n-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from "vue-router";
+import { NButton } from "naive-ui";
+const router = useRouter();
+function goHome() {
+  router.push("/");
+}
+</script>
+
+<style lang="scss" scoped>
+.page-container {
+  width: 100%;
+  border-radius: 4px;
+  padding: 50px 0;
+  height: 100vh;
+
+  .text-center {
+    color: #666;
+    padding: 20px 0;
+    text-align: center;
+    p {
+      margin-bottom: 20px;
+    }
+  }
+
+  img {
+    width: 350px;
+    margin: 0 auto;
+  }
+}
+</style>
