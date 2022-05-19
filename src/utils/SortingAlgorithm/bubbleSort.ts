f3d8f0bb0479c4e49a7432548f95964ff0da60f9
Cao Zhijie <1051029537@qq.com>
2022-05-19 20:20:28 +0800

    feat 改进冒泡排序，每执行一次都停止一会再执行下一步

--- src/utils/SortingAlgorithm/bubbleSort.ts
+++ src/utils/SortingAlgorithm/bubbleSort.ts
@@ -1,21 +1,52 @@
 /**
  * 冒泡排序
- * @param {number[]} arr
+ * @param {ISortItem[]} arr
+ * @param {number} delay
  * @return {number[]}
  */
+import { ISortItem } from "@/types/Sort";
 
-const bubbleSort = (arr: number[]): number[] => {
+const bubbleSort = (arr: ISortItem[], delay = 700): ISortItem[] => {
   const l = arr.length;
-  let temp;
-  for (let i = 0; i < l; i++) {
-    for (let j = l - 1 - i; j < l; j++) {
-      if (arr[j] > arr[j + 1]) {
+  let temp,
+    i = 0,
+    j = 0;
+
+  //内层循环
+  const innerLoop = () => {
+    if (j >= l - 1 - i) {
+      arr[j].status = 2;
+      j = 0;
+      i++;
+      outLoop();
+      return false;
+    }
+    arr[j].status = 1;
+    arr[j + 1].status = 1;
+    setTimeout(() => {
+      if (arr[j].value > arr[j + 1].value) {
         temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
       }
+      setTimeout(() => {
+        arr[j].status = 0;
+        arr[j + 1].status = 0;
+        j++;
+        innerLoop();
+      }, delay);
+    }, 300);
+  };
+
+  //外层循环
+  const outLoop = () => {
+    if (i > l) {
+      return false;
     }
-  }
+    innerLoop();
+  };
+
+  outLoop();
   return arr;
 };
 
