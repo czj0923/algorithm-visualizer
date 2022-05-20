6e2cdbff61b0f26f0aa7e5df5857cff94e9b2298
Cao Zhijie <1051029537@qq.com>
2022-05-20 21:47:07 +0800

    feat 改造选择排序 分为内层函数和外层函数

--- src/utils/SortingAlgorithm/selectionSort.ts
+++ src/utils/SortingAlgorithm/selectionSort.ts
@@ -1,15 +1,56 @@
 /**
  * 选择排序
- * @param {number[]} arr
+ * @param {ISortItem[]} arr
+ * @param {number} delay
  * @return {number[]}
  */
+import { ISortItem } from "@/types/Sort";
 
-const selectionSort = (arr: number[]): number[] => {
+const selectionSort = (arr: ISortItem[], delay = 700): ISortItem[] => {
   const l = arr.length;
-  let minIndex, temp;
-  for (let i = 0; i < l - 1; i++) {
+  let minIndex = 0,
+    temp,
+    i = 0,
+    j = 0;
+  const innerLoop = () => {
+    if (j >= l) {
+      minIndex < l && (arr[minIndex].status = 2);
+      outLoop();
+      return false;
+    }
+    arr[minIndex].status = 3;
+    arr[j].status = 1;
+    setTimeout(() => {
+      arr[minIndex].status = 0;
+      arr[j].status = 0;
+      if (arr[minIndex].value > arr[j].value) {
+        minIndex = j;
+      }
+      j++;
+      innerLoop();
+    }, delay);
+  };
+  const outLoop = (isFirst?: boolean) => {
+    if (i > l - 1) {
+      return false;
+    }
+    if (!isFirst) {
+      temp = arr[minIndex];
+      arr[minIndex] = arr[i];
+      arr[i] = temp;
+
+      i++;
+    }
     minIndex = i;
-    for (let j = i; j < l; j++) {
+    j = i + 1;
+
+    innerLoop();
+  };
+  outLoop(true);
+  /*for (let i = 0; i < l - 1; i++) {
+    minIndex = i;
+
+    for (let j = i+1; j < l; j++) {
       if (arr[minIndex] > arr[j]) {
         minIndex = j;
       }
@@ -17,7 +58,7 @@ const selectionSort = (arr: number[]): number[] => {
     temp = arr[minIndex];
     arr[minIndex] = arr[i];
     arr[i] = temp;
-  }
+  }*/
   return arr;
 };
 
